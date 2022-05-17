a21aed3508a75a1a25fe9ee90354a824b23694f8
Alejandro Andrade <meuxdev@gmail.com>
2022-05-17 01:14:13 -0500

    Interfaces - The contracts

--- src/inheritance/class.go
+++ src/inheritance/class.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type Person struct {
 	name string
-	age  int
+	age  int32
 }
 
 type Employee struct {
@@ -15,6 +15,44 @@ type Employee struct {
 type FullTimeEmployee struct {
 	Person
 	Employee
+	enDate string
+}
+
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full time employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int32
+}
+
+func NewTemporaryEmployee(id, age, taxRate int32, name string) *TemporaryEmployee {
+
+	return &TemporaryEmployee{
+		Person: Person{
+			name: name,
+			age:  age,
+		},
+		Employee: Employee{
+			id: id,
+		},
+		taxRate: taxRate,
+	}
+}
+
+func (tempEmployee TemporaryEmployee) getMessage() string {
+	return fmt.Sprintf("***************************************\nEmployee Type: Temporary Employee\n Name: %s\n Age: %d Years Old\n Employee ID: %d\n Tax Rate: %d\n****************************************", tempEmployee.name, tempEmployee.age, tempEmployee.id, tempEmployee.taxRate)
+}
+
+type PrintInfo interface {
+	getMessage() string
+}
+
+// Implementing the interface
+func EmployeePrintMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
 }
 
 func GetMessage(p Person) {
@@ -29,5 +67,8 @@ func main() {
 	fmt.Printf("%+v\n", ftEmployee)
 	fmt.Printf("%v\n", ftEmployee)
 	GetMessage(ftEmployee.Person) // Full time employee has the person in his composition
+	tEmployee := NewTemporaryEmployee(23, 32, 12, "Richard Thomson")
+	EmployeePrintMessage(*tEmployee)
+	EmployeePrintMessage(ftEmployee)
 
 }
