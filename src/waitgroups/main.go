0d89734d154792cdd9815468c2a8e8701148cea4
Alejandro Andrade <meuxdev@gmail.com>
2022-06-02 22:53:41 -0500

    Adding the notes of the wait group

--- src/waitgroups/main.go
+++ src/waitgroups/main.go
@@ -11,15 +11,18 @@ func main() {
 	var wg sync.WaitGroup
 
 	for i := 0; i < 10; i++ {
+		// adding a task to the wait group
 		wg.Add(1)
 		go doSomething(i, &wg)
 	}
 
+	// waits until the index of the tasks in the waitgroup is 0.
 	wg.Wait()
 }
 
 func doSomething(i int, wg *sync.WaitGroup) {
 	// defer exec. that at the end of the func.
+	// Done substracs 1 to the index of tasks of the wait group
 	defer wg.Done()
 	fmt.Printf("Starting the task number %d...\n", i)
 	time.Sleep(time.Second * 2)
